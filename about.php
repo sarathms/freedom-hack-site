019f92acc96a7c90995c24e48d1e3367e36f6508
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-05 21:11:35 +0530

    Add about content

--- about.php
+++ about.php
@@ -20,13 +20,15 @@ include 'includes/head.php';
   <!-- Page Content -->
   <div class="container">
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris convallis sem eu justo iaculis feugiat. Ut faucibus euismod sapien, vel viverra leo malesuada nec. Duis maximus augue eros, non iaculis diam placerat non. Ut erat ante, commodo quis turpis quis, auctor pellentesque arcu. Phasellus viverra ipsum neque, eget sagittis nunc porttitor quis. Pellentesque fringilla lacus et ligula congue, ac egestas nibh venenatis. Phasellus nec suscipit ipsum, et ullamcorper urna. Phasellus eu arcu vitae ante fermentum interdum. Aenean id neque lorem. Integer mauris erat, molestie in mi a, vehicula egestas risus. Donec non condimentum risus.</p>
+    <p>Freedom Hack is a 24 hour Hackathon which is being organised by Free Software Movement Karnataka(FSMK) in association with Hacker Earth. Freedom Hack is being organised in the memory of the late Aaron Swartz, an Internet activist who fought for the open and free Internet platform. You can read more about Aaron Swartz <a href="who-was-aaron-swartz">here</a>.</p>
 
-    <p>Maecenas eu enim nunc. Nullam mollis sem non lectus tincidunt, eget gravida libero rutrum. In faucibus lacus at ipsum gravida dignissim eget a odio. Nulla tempus lacus eget lacus vulputate pellentesque. Nam nec odio sit amet ex rhoncus ornare. Integer congue bibendum fringilla. In vehicula consectetur lacus vel elementum. Sed sollicitudin vestibulum lorem, vitae iaculis quam varius a. Pellentesque et mi egestas, egestas ipsum sit amet, bibendum libero. Nunc nisl neque, malesuada et feugiat vel, malesuada non ligula. Nulla facilisi. Proin at nisi sed mi imperdiet commodo. Phasellus tincidunt euismod quam vestibulum pharetra. Etiam consectetur tortor quis quam aliquet, eget egestas purus feugiat. Nullam iaculis, purus nec posuere sollicitudin, lorem lectus congue massa, vel vulputate est ante vitae justo. Quisque ut ante dignissim, eleifend velit a, accumsan turpis.</p>
+    <p>January is celebrated as the month of Data Privacy and so, we call all hackers to contribute to make the Internet more open.</p>
 
-    <p>Fusce malesuada sed quam vitae egestas. Vestibulum hendrerit diam a dui varius, non auctor sem sodales. Maecenas mollis mi sit amet eros dignissim, at varius risus euismod. Sed commodo tortor at sapien facilisis dictum. Aliquam enim eros, facilisis vel pharetra non, pulvinar a libero. Nunc vehicula, velit ac vestibulum convallis, nisi arcu ultricies lorem, non iaculis augue turpis in arcu. Ut in imperdiet nibh. Nullam sollicitudin erat elementum, lobortis dolor nec, bibendum eros. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In dolor enim, dignissim non varius a, sodales a erat. Donec fermentum velit eu accumsan tincidunt. Morbi quis ex magna.</p>
+    <p>The theme of the event is <b>Fork The Internet</b> and all the solutions to the problem statements should be ways to achieve this.</p>
 
-    <p>Etiam congue purus enim, at placerat nibh faucibus eget. Nunc non sollicitudin velit. Donec rutrum hendrerit ipsum, ut elementum eros cursus eget. Nulla facilisi. Cras fermentum arcu ut nisl auctor convallis. Nullam dui lacus, maximus eu magna id, mattis tempor odio. Nam tempus porta magna accumsan ultrices. Donec dignissim turpis vel rhoncus iaculis. </p>
+    <p>Freedom Hack is an initiative of Free Software Movement Karnataka (FSMK), a non profit organisation which strives to promote the use and development of open source software.</p>
+
+    <p><a href="register">Register now!</a></p>
   </div>
   <!-- /.container -->
   <?php
