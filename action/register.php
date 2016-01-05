ce2faf5548b5faeeb942e89a2250cf55664ce3b0
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 00:09:10 +0530

    Update register.php

--- action/register.php
+++ action/register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = mysqli_connect('localhost','root','cityafterme','freedomhack');
+$conn = mysqli_connect('localhost','root','','freedomhack');
 
 $name = $_POST['name'];
 $name = $conn->real_escape_string($name);
