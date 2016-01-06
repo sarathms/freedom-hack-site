9333c70379f11f939182ea01b4b4ff41e822be05
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 18:16:05 +0530

    Change schedule layout

--- schedule.php
+++ schedule.php
@@ -13,8 +13,8 @@ include 'includes/head.php';
 
   <!-- Header -->
   <header class="jumbotron hero-spacer">
-<h1>Schedule</h1>
-<p>It's going to be a packed 24 hours!</p>
+    <h1>Schedule</h1>
+    <p>It's going to be a packed 24 hours!</p>
   </header>
 
   <!--  Content -->
@@ -23,138 +23,253 @@ include 'includes/head.php';
       <table class="table" style="width: 50%;">
         <thead>
           <th>
-            Date
+            January 9th, 2016
           </th>
           <th>
-            Time
-          </th>
-          <th>
-            Event
+
           </th>
         </thead>
         <tbody>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            12:00 PM
-          </td>
-          <td>
-            Open for registrations
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            01:00 PM
-          </td>
-          <td>
-            Let the hacking begin!
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            03:30 PM
-          </td>
-          <td>
-Snack break
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            06:30 PM
-          </td>
-          <td>
-Lightning Talk 1
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            08:30 PM
-          </td>
-          <td>
-Dinner break
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            11:30 PM
-          </td>
-          <td>
-Lightning Talk 2
-          </td>
-</tr>
-<tr>
-          <td>
-            10/01/2016
-          </td>
-          <td>
-            09:00 AM
-          </td>
-          <td>
-Breakfast
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            12:00 PM
-          </td>
-          <td>
-            Final Wrap Up call
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            01:00 PM
-          </td>
-          <td>
-Stop hacking
-          </td>
-</tr>
-<tr>
-          <td>
-            09/01/2016
-          </td>
-          <td>
-            01:15 PM
-          </td>
-          <td>
-Pitch your hacks
-          </td>
-</tr>
+          <tr>
+
+            <td>
+              12:00 PM
+            </td>
+            <td>
+              Open for registrations
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              01:00 PM
+            </td>
+            <td>
+              Let the hacking begin!
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              03:30 PM
+            </td>
+            <td>
+              Snack break
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              06:30 PM
+            </td>
+            <td>
+              Lightning Talk 1
+            </td>
+          </tr>
+          <tr>
+            <td>
+              08:30 PM
+            </td>
+            <td>
+              Dinner break
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              11:30 PM
+            </td>
+            <td>
+              Lightning Talk 2
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table class="table" style="width: 50%;">
+        <thead>
+          <th>
+            January 10th, 2016
+          </th>
+          <th>
+
+          </th>
+        </thead>
+        <tbody>
+          <tr>
+
+            <td>
+              09:00 AM
+            </td>
+            <td>
+              Breakfast
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              12:00 PM
+            </td>
+            <td>
+              Final Wrap Up call
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              01:00 PM
+            </td>
+            <td>
+              Stop hacking
+            </td>
+          </tr>
+          <tr>
+
+            <td>
+              01:15 PM
+            </td>
+            <td>
+              Pitch your hacks
+            </td>
+          </tr>
 
         </tbody>
       </table>
-    </center>
-  </div>
-  <!-- /.container -->
-  <?php
-  include 'includes/footer.php';
-  ?>
-  <?php
-  include 'includes/js.php';
-  ?>
+      <!--<table class="table" style="width: 50%;">
+      <thead>
+      <th>
+      Date
+    </th>
+    <th>
+    Time
+  </th>
+  <th>
+  Event
+</th>
+</thead>
+<tbody>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+12:00 PM
+</td>
+<td>
+Open for registrations
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+01:00 PM
+</td>
+<td>
+Let the hacking begin!
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+03:30 PM
+</td>
+<td>
+Snack break
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+06:30 PM
+</td>
+<td>
+Lightning Talk 1
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+08:30 PM
+</td>
+<td>
+Dinner break
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+11:30 PM
+</td>
+<td>
+Lightning Talk 2
+</td>
+</tr>
+<tr>
+<td>
+10/01/2016
+</td>
+<td>
+09:00 AM
+</td>
+<td>
+Breakfast
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+12:00 PM
+</td>
+<td>
+Final Wrap Up call
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+01:00 PM
+</td>
+<td>
+Stop hacking
+</td>
+</tr>
+<tr>
+<td>
+09/01/2016
+</td>
+<td>
+01:15 PM
+</td>
+<td>
+Pitch your hacks
+</td>
+</tr>
+
+</tbody>
+</table>-->
+</center>
+</div>
+<!-- /.container -->
+<?php
+include 'includes/footer.php';
+?>
+<?php
+include 'includes/js.php';
+?>
 </body>
 
 </html>
