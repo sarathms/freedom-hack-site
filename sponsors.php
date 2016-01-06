e4c1955a36a8a71121f186f7c50ee32c7f0aa212
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 18:55:47 +0530

    Add sponsor links

--- sponsors.php
+++ sponsors.php
@@ -22,17 +22,17 @@ include 'includes/head.php';
     <div class="row text-center">
       <div class="col-md-6 col-sm-12 hero-feature">
         <div class="thumbnail" style="border: 0;">
-          <div class="caption">
+          <div class="caption" style="cursor: pointer;" onclick="window.open('http://fsmk.org');">
             <img src="images/fsmklogo-black.png" width="15%">
-            <h2>FSMK</h2>
+            <h2 style="font-size: 18px;">FSMK</h2>
           </div>
         </div>
       </div>
       <div class="col-md-6 col-sm-12 hero-feature">
         <div class="thumbnail" style="border: 0;">
-          <div class="caption">
+          <div class="caption" style="cursor: pointer;" onclick="window.open('http://hackerearth.com');">
             <img src="images/he-logo.png" width="17%">
-            <h2>HackerEarth</h2>
+            <h2 style="font-size: 18px;">HackerEarth</h2>
           </div>
         </div>
       </div>
