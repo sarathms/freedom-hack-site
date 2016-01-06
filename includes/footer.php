29833b4e86a610a2c7b854f68ecf90917966b04a
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 17:54:08 +0530

    Update heart to red

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
     <footer>
       <div class="row">
         <div class="col-lg-12">
-          <p>Made with <i class="fa fa-heart"></i> by FSMK</p>
+          <p>Made with <i style="color: #ff0000;" class="fa fa-heart"></i> by FSMK</p>
           <!--<p><a href="http://fsmk.org" target="_blank"><img src="images/fsmklogo-white.png" width=5%></a></p>-->
         </div>
       </div>
