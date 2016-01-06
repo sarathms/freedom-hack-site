fc3e9f06a0a4165bef40fddb1c83c532ddf31963
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 16:57:01 +0530

    Add og tags and image

--- includes/head.php
+++ includes/head.php
@@ -1,25 +1,34 @@
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="FreedomHack">
-<meta name="keywords" content="freedom,hack,fsmk,freedomhack,hackerearth,hackathon,aaron,swartz">
-    <meta name="author" content="Free Software Movement Karnataka">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="FreedomHack">
+  <meta name="keywords" content="freedom,hack,fsmk,freedomhack,hackerearth,hackathon,aaron,swartz">
+  <meta name="author" content="Free Software Movement Karnataka">
 
-    <title>FreedomHack</title>
+  !-- Facebook tags -->
+  <meta property="og:title" content="FreedomHack 2.0"/>
+  <meta property="og:description" content="FreedomHack- Aaron Swartz Memorial Hackathon"/>
+  <meta property="og:image" content="images/og-image.png" />
+  <meta property="og:url" content="https://freedomhack.fsmk.org/"/>
+  <meta property="og:site_name" content="FreedomHack"/>
+  <meta property="og:type" content="website"/>
+  <!-- end Facbook tags -->
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+  <title>FreedomHack</title>
 
-    <!-- Custom CSS -->
-    <link href="css/style.css" rel="stylesheet">
+  <!-- Bootstrap Core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <!-- Custom CSS -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
 </head>
