68689634f372c64114279938ad3c82ee77d7a277
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 12:21:21 +0530

    Push changes to main

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-lg-12">
 <p>FreedomHack is an initiative of <a href="http://fsmk.org" target="_blank">Free Software Movement Karnataka</a></p>
-<p><a href="http://fsmk.org" target="_blank"><img src="images/fsmklogo-white.png" width=5%></a></p>
+<!--<p><a href="http://fsmk.org" target="_blank"><img src="images/fsmklogo-white.png" width=5%></a></p>-->
     </div>
   </div>
 </footer>
