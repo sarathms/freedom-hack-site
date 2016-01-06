152b9419c15efa9f5501a100c5616ca1f70666a5
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 19:47:42 +0530

    Edit text

--- aaron.php
+++ aaron.php
@@ -18,26 +18,27 @@ include 'includes/head.php';
 
   <!-- Content -->
   <div class="container">
-    <p>
-      Aaron Swartz was a computer programmer, open source proponent and internet activist. He was a key contributor to many projects from a very young age. The list of projects he worked on is impressive, with big names like the RSS web feed format, the Markdown publishing format which is a favourite of bloggers, the organisation Creative Commons and the web framework web.py. Aaron also contributed to the development of the popular website Reddit, after his site Infogami merged with Reddit.
-    </p>
-    <p>
-      Aaron was involved in the campaign against SOPA (Stop Online Piracy Act), claiming this bill could potentially shut down entire websites and could stop Americans from communicating with each other.
-    </p>
-    <p>
-      Aaron commited suicide on January 11th, 2013, when he was being charged for data theft. The procecution was described as "the product of a criminal-justice system rife with intimidation and prosecutorial overreach" by his family and many others.
-    </p>
-    <p>
-      Aaron's life and his contributions to the world have been captured in the documentary The Internet's Own Boy, which has been embeded below.
-    </p>
+
+    <p>Aaron Swartz[1] was a free software hacker, Internet activist and like to consider himself a political entrepreneur. He was a key contributor to many notable projects from a very young age. At the age of 14, he was part of a committee that created the RSS web feed format, a standard used heavily on the Internet. He was one of the early developers and founding members of Creative Commons, the license used by billions of creative works, including Wikipedia articles. Beyond this, he was instrumental in the development of the Markdown format and development of the web.py framework. The portfolio of projects he worked on is unimaginably impressive and has a profound social impact.</p>
+
+    <p>Aaron also contributed to the development of the popular news sharing website Reddit, after his site Infogami merged with Reddit. Although he made millions and a well paying job from the sale of Reddit to bigger corporation, he couldn't survive in the hostile corporate environment, neither did he let the money change his motivations and led a simple life.[2]</p>
+
+    <p>Aaron spearheaded in the landmark campaign against SOPA (Stop Online Piracy Act), claiming this bill could potentially shut down entire websites and could stop Americans from communicating with each other. As a result of the campaign, the US Congress ended up shelving the bill indefinitely.[3]</p>
+
+    <p>Aaron committed suicide on January 11th, 2013, after being charged for data theft. The prosecution was described as "the product of a criminal-justice system rife with intimidation and prosecutorial overreach" by his family and many others.</p>
+
+    <p>Aaron's life and his contributions to the world have been captured in the heart-melting, yet inspiring documentary The Internet's Own Boy, which is released in his memory under the Attribution-NonCommercial-ShareAlike 4.0 International License.[4]</p>
     <p>
       <div class="video-wrapper">
         <iframe src="https://archive.org/embed/TheInternetsOwnBoyTheStoryOfAaronSwartz" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>
       </div>
     </p>
     <p>
-      Read more about Aaron Swartz <a href="https://en.wikipedia.org/wiki/Aaron_Swartz" target="_blank">here</a>.
-    </p>
+      [1] <a href="https://en.wikipedia.org/wiki/Aaron_Swartz" target="_blank">https://en.wikipedia.org/wiki/Aaron_Swartz</a><br>
+      [2] <a href="http://www.aaronsw.com/weblog/everybodysays" target="_blank">http://www.aaronsw.com/weblog/everybodysays</a><br>
+      [3] <a href="http://www.democracynow.org/2013/1/14/freedom_to_connect_aaron_swartz_1986" target="_blank">http://www.democracynow.org/2013/1/14/freedom_to_connect_aaron_swartz_1986</a><br>
+      [4] <a href="https://archive.org/details/TheInternetsOwnBoyTheStoryOfAaronSwartz" target="_blank">https://archive.org/details/TheInternetsOwnBoyTheStoryOfAaronSwartz</a>
+</p>
   </div>
 
   <!-- /.container -->
