91c9327f1e23d2618d1495bbdab7181a70c9850c
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 17:12:12 +0530

    Update FreedomHack to FreedomHack 2016

--- about.php
+++ about.php
@@ -14,13 +14,13 @@ include 'includes/head.php';
   <!-- Header for the page -->
   <header class="jumbotron hero-spacer">
     <h1>About</h1>
-    <p>Who are we and why are we doing FreedomHack?</p>
+    <p>Who are we and why are we doing FreedomHack 2016?</p>
   </header>
 
   <!-- Page Content -->
   <div class="container">
 
-    <p>FreedomHack is a 24 hour Hackathon which is being organised by Free Software Movement Karnataka (FSMK) in association with HackerEarth. FreedomHack is being organised in the memory of the late Aaron Swartz, an Internet activist who fought for the open and free Internet platform. You can read more about Aaron Swartz <a href="who-was-aaron-swartz">here</a>.</p>
+    <p>FreedomHack 2016 is a 24 hour Hackathon which is being organised by Free Software Movement Karnataka (FSMK) in association with HackerEarth. FreedomHack 2016 is being organised in the memory of the late Aaron Swartz, an Internet activist who fought for the open and free Internet platform. You can read more about Aaron Swartz <a href="who-was-aaron-swartz">here</a>.</p>
 
     <p>January is celebrated as the month of Data Privacy and so, we call all hackers to contribute to make the Internet more open.</p>
 
