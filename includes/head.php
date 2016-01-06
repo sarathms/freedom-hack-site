ad1a39ee9e44bde334d1424a79cfc739f2595eb4
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-06 07:36:42 +0530

    Add meta tags

--- includes/head.php
+++ includes/head.php
@@ -3,8 +3,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="FreedomHack">
+<meta name="keywords" content="freedom,hack,fsmk,freedomhack,hackerearth,hackathon,aaron,swartz">
+    <meta name="author" content="Free Software Movement Karnataka">
 
     <title>Freedom Hack</title>
 
