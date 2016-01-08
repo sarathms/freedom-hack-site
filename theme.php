64875ce6ccc5f6a6de6aa322c1197c427ffa593c
Aditya Saky <paradoxdjell@users.noreply.github.com>
2016-01-08 14:23:31 +0530

    Reorganise content

--- theme.php
+++ theme.php
@@ -24,64 +24,63 @@ include 'includes/head.php';
   <!-- Page Content -->
   <div class="container">
 
-    <p>
-      The idea is to fork the internet. These are the various themes we believe will help us achieve that.
-    </p>
+    <h2>Problem Statement</h2>
 
-    <p>
-      1. <b>Communication app via mesh network</b><br>
-      Networks do not allow two phones to talk to each other directly. We need a way to allow two phone users to talk to each other using community band channels via low overhead networks.  The communication channel should support end-to-end encryption and should be secure. This could be a WhatsApp/Telegram replacement.
-    </p>
+    <p id="statement">Help the rural people who are currently not connected
+      to the Internet by  providing them connectivity via ad-hoc networks</p>
 
-    <p>
-      2. <b>Mobile app for citizen whistle-blowing</b><br>
-      Discussed in detail <a href="https://discuss.fsmk.org/t/mobile-app-for-citizen-whistle-blowing-celebrating-aaron-swartz/156" target="_blank">here</a>.
-    </p>
+      <h2>Description</h2>
 
-    <p>
-      3. <b>Features for federated social networks</b><br>
-      Discussed in detail <a href="https://discuss.fsmk.org/t/federation-on-discourse/95" target="_blank">here</a>.
-    </p>
+      <p>Connectivity needs to be provided in the form of ad hoc, distributed
+        networks. Once connectivity is established, services that run on these
+        networks need to be designed. These services must be benefetial to the
+        poor/farmers/fishermen/etc.</p>
 
-    <p>
-      4. <b>Mobile app for Discourse</b><br>
-      Discourse is web based and having a mobile app will improve adoption.
-    </p>
+        <p>Your hack can be either the implementation of the ad hoc network itself
+          or a service that runs on such networks. If designing a service, keep in
+          mind that it should function with little/no access to the Internet. A
+          localised economy must be created and used if applicable.</p>
+          
+          <p>
+            1. <b>Communication app via mesh network</b><br>
+            Networks do not allow two phones to talk to each other directly. We need a way to allow two phone users to talk to each other using community band channels via low overhead networks.  The communication channel should support end-to-end encryption and should be secure. This could be a WhatsApp/Telegram replacement.
+          </p>
 
-    <p>
-      5. <b>Improvements to Tor and its plugins</b>
-    </p>
+          <p>
+            2. <b>Mobile app for citizen whistle-blowing</b><br>
+            Discussed in detail <a href="https://discuss.fsmk.org/t/mobile-app-for-citizen-whistle-blowing-celebrating-aaron-swartz/156" target="_blank">here</a>.
+          </p>
 
-    <p>
-      6. <b>Improvements to Diaspora / Kontalk / XMPP</b>
-    </p>
+          <p>
+            3. <b>Features for federated social networks</b><br>
+            Discussed in detail <a href="https://discuss.fsmk.org/t/federation-on-discourse/95" target="_blank">here</a>.
+          </p>
 
-    <p>
-      7. <b>Improvements to Freedom Box</b>
-    </p>
+          <p>
+            4. <b>Mobile app for Discourse</b><br>
+            Discourse is web based and having a mobile app will improve adoption.
+          </p>
 
-    <p id="statement"><b>Problem statement:</b><i> Help the rural people who are currently not connected
-    			to the Internet by  providing them connectivity via ad-hoc networks</i> </p>
+          <p>
+            5. <b>Improvements to Tor and its plugins</b>
+          </p>
 
-    <p><b>Description: </b>Connectivity needs to be provided in the form of ad hoc, distributed
-    networks. Once connectivity is established, services that run on these
-    networks need to be designed. These services must be benefetial to the
-    poor/farmers/fishermen/etc.</p>
+          <p>
+            6. <b>Improvements to Diaspora / Kontalk / XMPP</b>
+          </p>
 
-    <p>Your hack can be either the implementation of the ad hoc network itself
-    or a service that runs on such networks. If designing a service, keep in
-    mind that it should function with little/no access to the Internet. A
-    localised economy must be created and used if applicable.</p>
+          <p>
+            7. <b>Improvements to Freedom Box</b>
+          </p>
 
+        </div>
+        <!-- /.container -->
+        <?php
+        include 'includes/footer.php';
+        ?>
+        <?php
+        include 'includes/js.php';
+        ?>
+      </body>
 
-  </div>
-  <!-- /.container -->
-  <?php
-  include 'includes/footer.php';
-  ?>
-  <?php
-  include 'includes/js.php';
-  ?>
-</body>
-
-</html>
+      </html>
